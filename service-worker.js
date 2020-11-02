73060231878bd9e62f7778bce3f190316879e727
Vitalik <ztx25sss@gmail.com>
2020-11-02 20:00:41 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.9f0c4b2dbe45a3c169f4e6419395d685.js"
 );
 
 self.addEventListener('message', (event) => {
